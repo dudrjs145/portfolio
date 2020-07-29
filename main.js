131b1656cf28f7676e5457fe0560dc4699b2c1f4
dudrjs145 <dudrjs145@naver.com>
2020-07-30 00:01:17 +0900

    JS Home transparent

--- main.js
+++ main.js
@@ -36,3 +36,10 @@ function scrollIntoView(selector) {
     behavior: "smooth",
   });
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
